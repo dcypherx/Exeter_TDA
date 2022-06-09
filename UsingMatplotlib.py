495ac9629901113833e7dc5b8883230004318b11
dcypherx <106881507+dcypherx@users.noreply.github.com>
2022-06-09 10:56:25 +0530

    Add files via upload

---
+++ UsingMatplotlib.py
@@ -0,0 +1,25 @@
+import py4cytoscape as p4c
+
+# Import the class
+import kmapper as km
+
+# Some sample data
+from sklearn import datasets
+data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
+
+# Initialize
+mapper = km.KeplerMapper(verbose=1)
+
+# Fit to and transform the data
+projected_data = mapper.fit_transform(data, projection=[0,1]) # X-Y axis
+
+# Create a cover with 10 elements
+cover = km.Cover(n_cubes=10)
+
+# Create dictionary called 'graph' with nodes, edges and meta-information
+graph = mapper.map(projected_data, data, cover=cover)
+
+import matplotlib.pyplot as plt
+
+km.draw_matplotlib(graph)
+plt.savefig("mygraph.png")
